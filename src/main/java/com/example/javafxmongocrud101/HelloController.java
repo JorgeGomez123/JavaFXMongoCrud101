e87b630ff108f4d181999d2d57dd52a96624b84e
Jorge <Jorge@campusfp.es>
2024-05-16 13:50:31 +0200

    Capa visual mostrar en ListView

--- src/main/java/com/example/javafxmongocrud101/HelloController.java
+++ src/main/java/com/example/javafxmongocrud101/HelloController.java
@@ -2,13 +2,19 @@ package com.example.javafxmongocrud101;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private ListView lv_datos;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+    @FXML
+    protected void mostrar(){
+        
+    }
+}//Cierra la clase
